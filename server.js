325681175dce06ffefb66926d96a17af6a174498
Zachary Smith <billyzacsmith@gmail.com>
2016-03-17 16:54:06 -0600

    Use my watson-personality module to look up and respond with a personality profile.

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 var Express = require('express')
+var personality = require('watson-personality')
 
 var app = Express()
 var PORT = process.env.PORT || 3000
 
 app.get('/', function(request, response) {
-  console.log(request);
-  response.send('hey')
+
+  personality('@billyzacsmith')
+    .then(function(personalityProfile) {
+      console.log(personalityProfile.traits)
+      console.log(personalityProfile.needs)
+      response.send(personalityProfile.needs)
+    })
+    .catch(function(error) {
+      console.log(error)
+    })
 })
 
 app.listen(PORT, function() {
